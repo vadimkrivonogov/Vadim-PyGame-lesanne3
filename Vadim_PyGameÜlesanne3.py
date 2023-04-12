3d7f1ee48dcbfb1d7194faaaeca8e9ba70147a2f
opilane <opilane@HP-CZC2349HT2>
2023-04-12 12:23:55 +0300

    Add project files.

---
+++ Vadim_PyGameÜlesanne3.py
@@ -0,0 +1,42 @@
+﻿from lib2to3 import pygram
+import pygame
+import random
+import sys
+
+def Maja(x,y,laius,korgus,pind,varv):
+    punktid=[(x,y- ((3/4.0) * korgus)), (x,y), (x+laius,y), (x+laius, y-(3/4.0) * korgus), (x,y- ((3/4.0) * korgus)), (x+laius/2.0, y- korgus), (x+laius,y-(3/4.0)*korgus)]
+
+r=random.randint(0,255)
+g=random.randint(0,255)
+b=random.randint(0,255)
+fon=[r,g,b]
+
+r=random.randint(0,255)
+g=random.randint(0,255)
+b=random.randint(0,255)
+majavarv=[r,g,b]
+pind=pygame.display.set_mode([640,480])
+pygame.display.set_caption("Ülesanne 3")
+pind.fill(fon)
+
+for i in range(10):
+    r=random.randint(0,255)
+    g=random.randint(0,255)
+    b=random.randint(0,255)
+    varv=[r,g,b]
+
+    laius=random.randint(1,80)
+    korgus=random.randint(1,80)
+
+    x=random.randint(0,640-laius)
+    y=random.randint(0,480-korgus)
+    pygame.draw.rect(pind,varv,[x,y,laius,korgus])
+
+    pygame.display.flip()
+
+while True:
+    event=pygame.event.poll()
+    if event.type==pygame.QUIT:
+        sys.exit()
+pygame.quit()
+
---
+++ module1.py
@@ -0,0 +1,35 @@
+﻿from lib2to3 import pygram
+import pygame
+import random
+import sys
+
+r=random.randit(0,255)
+g=random.randit(0,255)
+b=random.randit(0,255)
+
+fon=[r,g,b]
+
+pind=pygame.display.set_mode([640,480])
+pygame.display.set_caption("Ülesanne 3")
+pind.fill(fon)
+
+for i in range(10):
+    r=random.randit(0,255)
+    g=random.randit(0,255)
+    b=random.randit(0,255)
+    varv=[r,g,b]
+
+    laius=random.ranint(1,80)
+
+    x=random.randint(0,610)
+    y=random.randint(0,450)
+    pygame.draw.rect(pind,varv,[x,y,30,30])
+
+    pygame.display.flip()
+
+while True:
+    event=pygame.event.poll()
+    if event.type==pygame.QUIT:
+        sys.exit()
+pygame.quit()
+
